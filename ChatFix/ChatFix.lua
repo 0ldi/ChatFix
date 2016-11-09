e2656710d5be5a7a97ce1a4e970d48b40346521b
0ldi <0ldi@users.noreply.github.com>
2016-11-09 21:09:31 +0300

    Add files via upload

--- ChatFix/ChatFix.lua
+++ ChatFix/ChatFix.lua
@@ -3,22 +3,64 @@ Frame:RegisterEvent("PLAYER_ENTERING_WORLD");
 
 local start_time = GetTime();
 local is_in_world = false;
-local is_in_reported = false;
-local leaved = false;
+local is_in_channels = false;
+local left_channels = false
 
  Frame:SetScript("OnUpdate", function(self, elapsed)
-	if is_in_world and not is_in_reported then
+	if is_in_world and not is_in_channels then
 		local elapsed = GetTime()  -  start_time;
-		if elapsed >= 15 and not leaved then
-			LeaveChannelByName("reported")
-			leaved = true
-			DEFAULT_CHAT_FRAME:AddMessage("Leaving reported channel.")
-		end
-
-		if elapsed >= 20  and leaved then
-			JoinChannel("reported")
-			DEFAULT_CHAT_FRAME:AddMessage("Joining reported channel.")
-			is_in_reported = true;
+		
+		-- seconds after PLAYER_ENTERING_WORLD fires.
+		-- If any channels you join below are suddenly numbered
+		-- before the usual default channels (/1 general, /2 trade etc),
+		-- you can try to increase the number of seconds so the game 
+		-- has time to join the default channels before the timer runs out.
+		if elapsed >= 15 and left_channels then
+		
+			-- If you have other channels you want to automatically join
+			-- every time you log on, add them here like so:
+			-- JoinChannel("myOtherChannel");
+			JoinChannel("World");
+			JoinChannel("Trade");
+			JoinChannel("General");
+			JoinChannel("LocalDefense");
+			--JoinChannel("WorldDefense");
+			ChatFrame_RemoveChannel(ChatFrame1, "World");
+			ChatFrame_RemoveChannel(ChatFrame2, "World");
+			ChatFrame_RemoveChannel(ChatFrame4, "World");
+			ChatFrame_RemoveChannel(ChatFrame5, "World");
+			ChatFrame_RemoveChannel(ChatFrame1, "Trade");
+			ChatFrame_RemoveChannel(ChatFrame2, "Trade");
+			ChatFrame_RemoveChannel(ChatFrame4, "Trade");
+			ChatFrame_RemoveChannel(ChatFrame5, "Trade");
+			ChatFrame_RemoveChannel(ChatFrame1, "General");
+			ChatFrame_RemoveChannel(ChatFrame2, "General");
+			ChatFrame_RemoveChannel(ChatFrame4, "General");
+			ChatFrame_RemoveChannel(ChatFrame5, "General");
+			ChatFrame_RemoveChannel(ChatFrame1, "LocalDefense");
+			ChatFrame_RemoveChannel(ChatFrame2, "LocalDefense");
+			ChatFrame_RemoveChannel(ChatFrame4, "LocalDefense");
+			ChatFrame_RemoveChannel(ChatFrame5, "LocalDefense");
+			--ChatFrame_RemoveChannel(ChatFrame1, "WorldDefense");
+			--ChatFrame_RemoveChannel(ChatFrame2, "WorldDefense");
+			--ChatFrame_RemoveChannel(ChatFrame4, "WorldDefense");
+			--ChatFrame_RemoveChannel(ChatFrame5, "WorldDefense");			
+			ChatFrame_AddChannel(ChatFrame3, "World");
+			ChatFrame_AddChannel(ChatFrame3, "Trade");
+			ChatFrame_AddChannel(ChatFrame3, "General");
+			ChatFrame_AddChannel(ChatFrame3, "LocalDefense");
+			--ChatFrame_AddChannel(ChatFrame3, "WorldDefense");
+			
+			is_in_channels = true;
+			
+		elseif elapsed >= 13 and not left_channels then
+			-- Leaves the channels specified below before re-joining them.
+			LeaveChannelByName("World");
+			LeaveChannelByName("Trade");
+			LeaveChannelByName("General");
+			LeaveChannelByName("LocalDefense");
+			LeaveChannelByName("WorldDefense");
+			left_channels = true;
 		end
 	end
 end)
@@ -33,4 +75,3 @@ function JoinChannel(name)
 	JoinChannelByName(name) 
 	ChatFrame_AddChannel(DEFAULT_CHAT_FRAME, name) 
 end
-
