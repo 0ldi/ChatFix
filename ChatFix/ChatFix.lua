58e4176594771e0b63af6ac2fe2c610b11f9f80e
0ldi <0ldi@users.noreply.github.com>
2016-11-10 12:15:42 +0300

    Add files via upload

--- ChatFix/ChatFix.lua
+++ ChatFix/ChatFix.lua
@@ -20,45 +20,45 @@ local left_channels = false;
 			-- If you have other channels you want to automatically join
 			-- every time you log on, add them here like so:
 			-- JoinChannel("myOtherChannel");
-			JoinChannel("World");
-			JoinChannel("Trade");
 			JoinChannel("General");
+			JoinChannel("Trade");
 			JoinChannel("LocalDefense");
+			JoinChannel("World");
 			--JoinChannel("WorldDefense");
-			ChatFrame_RemoveChannel(ChatFrame1, "World");
-			ChatFrame_RemoveChannel(ChatFrame2, "World");
-			ChatFrame_RemoveChannel(ChatFrame4, "World");
-			ChatFrame_RemoveChannel(ChatFrame5, "World");
-			ChatFrame_RemoveChannel(ChatFrame1, "Trade");
-			ChatFrame_RemoveChannel(ChatFrame2, "Trade");
-			ChatFrame_RemoveChannel(ChatFrame4, "Trade");
-			ChatFrame_RemoveChannel(ChatFrame5, "Trade");
 			ChatFrame_RemoveChannel(ChatFrame1, "General");
 			ChatFrame_RemoveChannel(ChatFrame2, "General");
 			ChatFrame_RemoveChannel(ChatFrame4, "General");
 			ChatFrame_RemoveChannel(ChatFrame5, "General");
+			ChatFrame_RemoveChannel(ChatFrame1, "Trade");
+			ChatFrame_RemoveChannel(ChatFrame2, "Trade");
+			ChatFrame_RemoveChannel(ChatFrame4, "Trade");
+			ChatFrame_RemoveChannel(ChatFrame5, "Trade");
 			ChatFrame_RemoveChannel(ChatFrame1, "LocalDefense");
 			ChatFrame_RemoveChannel(ChatFrame2, "LocalDefense");
 			ChatFrame_RemoveChannel(ChatFrame4, "LocalDefense");
 			ChatFrame_RemoveChannel(ChatFrame5, "LocalDefense");
+			ChatFrame_RemoveChannel(ChatFrame1, "World");
+			ChatFrame_RemoveChannel(ChatFrame2, "World");
+			ChatFrame_RemoveChannel(ChatFrame4, "World");
+			ChatFrame_RemoveChannel(ChatFrame5, "World");
 			--ChatFrame_RemoveChannel(ChatFrame1, "WorldDefense");
 			--ChatFrame_RemoveChannel(ChatFrame2, "WorldDefense");
 			--ChatFrame_RemoveChannel(ChatFrame4, "WorldDefense");
-			--ChatFrame_RemoveChannel(ChatFrame5, "WorldDefense");			
-			ChatFrame_AddChannel(ChatFrame3, "World");
-			ChatFrame_AddChannel(ChatFrame3, "Trade");
+			--ChatFrame_RemoveChannel(ChatFrame5, "WorldDefense");	
 			ChatFrame_AddChannel(ChatFrame3, "General");
-			ChatFrame_AddChannel(ChatFrame3, "LocalDefense");
+			ChatFrame_AddChannel(ChatFrame3, "Trade");
+			ChatFrame_AddChannel(ChatFrame3, "LocalDefense");		
+			ChatFrame_AddChannel(ChatFrame3, "World");
 			--ChatFrame_AddChannel(ChatFrame3, "WorldDefense");
 			
 			is_in_channels = true;
 			
 		elseif elapsed >= 13 and not left_channels then
 			-- Leaves the channels specified below before re-joining them.
-			LeaveChannelByName("World");
-			LeaveChannelByName("Trade");
 			LeaveChannelByName("General");
+			LeaveChannelByName("Trade");
 			LeaveChannelByName("LocalDefense");
+			LeaveChannelByName("World");
 			LeaveChannelByName("WorldDefense");
 			left_channels = true;
 		end
